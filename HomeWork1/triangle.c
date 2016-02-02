d67997f6ffd0433c0323494a6f73be6a32b2b84b
Scruf <ekozitski@hotmail.com>
2016-02-01 19:18:43 -0800

    Printing for even numbers are finished and tested

--- HomeWork1/triangle.c
+++ HomeWork1/triangle.c
@@ -5,29 +5,32 @@ void print_triangle(int size){
     for(int i=0;i<size;i++)
       dest[i]=' ';
     int counter=size;
-    if(size%2==0){
-
-      while(size>=0){
-        if(size>=0){
-          dest[size]='*';
-          dest[--size]='*';
-          for(int i=0;i<counter;i++)
-            printf("%c",dest[i]);
-        printf("\n");
-        }
-      }
-    }
-    while(size>=0){
-    dest[size]='*';
-      for(int i=0;i<counter;i++)
+  
+    if(size%2==1){
+      dest[--size]='*';
+      for(int i=0;i<counter;i++){
         printf("%c",dest[i]);
-    printf("\n");
-    size--;
+      }
+      printf("\n");
+      int first,second=0;
+      while(size>=0){
+          if(size==0)
+            break;
+          first = size - 1;
+          second = size - 2;
+          dest[first]='*';
+          dest[second]='*';
+
+          for(int i=0;i<counter;i++){
+            printf("%c",dest[i]);
+          }
+          printf("\n");
+          size = size - 2;
+        }
+
     }
 }
-
-
 int main(){
-  print_triangle(6);
+  print_triangle(7);
   return 0;
 }
