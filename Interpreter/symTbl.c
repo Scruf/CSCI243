1d63ade6ac9d8da7c77b3066420e4a638f6d9924
Scruf <ekozitski@hotmail.com>
2016-04-02 12:27:10 -0700

    Partial Implementation of Symbol table

--- Interpreter/symTbl.c
+++ Interpreter/symTbl.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "helper.h"
 static int number_of_symbols;
 static Symbol symbol[MAX_SYMBOLS];
+static Symbol createSymbol(Type type, char* varName, char* value);
+
 void buildTable(char filename){
         FILE *file = fopen(filename,"r");
         if (file==0) {
@@ -31,14 +34,14 @@ void buildTable(char filename){
                 }
                 counter++;
         }
-        number_of_elements=counter;
+        number_of_symbols=counter;
         fclose(file);
         return;
 }
 
 void dumpTable(void){
         printf("SYMBOL TABLE:\n");
-        for (int i=0; i<number_of_elements; i++) {
+        for (int i=0; i<number_of_symbols; i++) {
                 if(symbol[i].value.type==TYPE_INT) {
                         printf("Type int, Name: %s, Value: %d\n",symbol[i].varName,symbol[i].value.value.iVal);
                 }
@@ -49,5 +52,33 @@ void dumpTable(void){
 }
 
 Symbol* lookupTable(char variable[]){
-  for(int i=0;i<number)
+        for(int i=0; i<number) {
+                if(strcmp(variable, symbol[i].varName)==0) {
+                        return &symbol[i];
+                }
+        }
+        return 0;
+}
+int addTable(Symbol symbol1){
+        if (number_of_symbols<MAX_SYMBOLS) {
+                symbol[number_of_symbols]=symbol1;
+                number_of_symbols++;
+                return number_of_symbols;
+        }
+        else{
+                return 0;
+        }
+}
+Symbol createSymbol(Type type, char* varName, char* value){
+        struct Symbol temp;
+        strcpy(temp.varName,varName);
+        temp.value.type = type;
+        if(type==TYPE_INT) {
+                temp.value.iVal=str_to_int(value);
+
+        }
+        if(type==TYPE_DOUBLE) {
+                ns.value.dVal = atof(value);
+        }
+        return temp;
 }
