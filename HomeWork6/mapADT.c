5821bf4a221583379be138066e5e5b0dffb9a65f
Scruf <ekozitski@hotmail.com>
2016-04-19 18:37:56 -0400

    Fixed wrong memory allocation in  create map

--- HomeWork6/mapADT.c
+++ HomeWork6/mapADT.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
-struct mapADT{
+struct mapADT {
         void **keys;
         void **values;
         bool (*equals)(const void *a,const void *b);
@@ -12,22 +12,24 @@ typedef struct mapADT *MapADT;
 MapADT map_create( bool (*equals)(const void *a,const void *b) ){
         MapADT map;
         map = (MapADT)malloc(sizeof(struct mapADT));
-        if(map!=0){
-          map->equals = equals;
+        if(map!=0) {
+                map->equals = equals;
+                  return (map);
         }
-        return (map);
-        // MapADT map;
-        // if (equals!=0) {
-        //         map = (MapADT)malloc(sizeof(map));
-        //         map->equals = equals;
-        // }
-        // return map;
+        else{
+          return 0;
+        }
+
 
 }
 void map_destroy( MapADT map ){
-        free(map->keys);
-        free(map->values);
-        free(map->equals);
+        for(unsigned int i = 0; i < map->size; i++) {
+                free(map->keys[i]);
+                free(map->values[i]);
+                free(map->equals);
+                free(map);
+//  size--;
+        }
 }
 void map_clear( MapADT map ){
         map->size = 0;
@@ -43,21 +45,12 @@ bool map_contains(MapADT map, void *key){
         return false;
 }
 void* map_put( MapADT map, void *key, void *value){
-        if (map_contains(map,key)) {
-
-                const void *old = map_get(map,key);
-                void *r = (unsigned *)map_get(map,key);
-                r=value;
-                return (unsigned *)old;
-        }else{
-          
-                map->size++;
-                map->keys=realloc(map->keys,sizeof(void*)*map->size);
-                map->values=realloc(map->values,sizeof(void*)*map->size);
-                map->keys[map->size-1]=key;
-                map->values[map->size-1]=value;
-                return value;
-        }
+        map->size++;
+        map->keys=realloc(map->keys,sizeof(void*)*map->size);
+        map->values=realloc(map->values,sizeof(void*)*map->size);
+        map->keys[map->size-1]=key;
+        map->values[map->size-1]=value;
+        return value;
 }
 const void *map_get( MapADT map, void *key ){
         if (map_empty(map)) {
