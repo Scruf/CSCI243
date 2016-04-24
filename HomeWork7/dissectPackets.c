3c3cf9420fa7e171e6deaff3b36bde9500e60b59
Scruf <ekozitski@hotmail.com>
2016-04-24 10:30:52 -0400

    Reading number of packets from packet file

--- HomeWork7/dissectPackets.c
+++ HomeWork7/dissectPackets.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include<byteswap.h>
 
 int main(char argc, char *argv[]){
   if(argc==1){
     printf("No file was specified");
     exit(1);
    }
+  
   else{
     FILE *file;
     file = fopen(argv[1],"rb");
-    if(file==0)
-      printf("File does not exists");
-    else{
-      printf("File actually exists lol");
-    }
+      size_t buffer_size = 2048;
+      char *buffer;
+      int num;
+      fread(&num, sizeof(int),1,file);
+      buffer = (char*)malloc(buffer_size);
+      fread(buffer,sizeof(buffer_size),1,file);
+      printf("%d",num);
+      free(buffer);
+      fclose(file);
+   
+      
+    
   }
 }
