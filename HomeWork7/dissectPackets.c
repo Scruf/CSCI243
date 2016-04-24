ed9511d633f2cadb801e5b6db9543d8b8af4ea1f
Scruf <ekozitski@hotmail.com>
2016-04-24 11:13:28 -0400

    Added packet version

--- HomeWork7/dissectPackets.c
+++ HomeWork7/dissectPackets.c
@@ -14,12 +14,30 @@ int main(char argc, char *argv[]){
     file = fopen(argv[1],"rb");
       size_t buffer_size = 2048;
       char *buffer;
-      int num;
-      fread(&num, sizeof(int),1,file);
-      buffer = (char*)malloc(buffer_size);
-      fread(buffer,sizeof(buffer_size),1,file);
-      printf("%d",num);
-      free(buffer);
+      int number_of_packets;
+      fread(&number_of_packets, sizeof(int),1,file);
+      if(number_of_packets==0){
+	printf("No packets were found\n");
+	exit(1);
+      }
+      else{
+	printf("==== File %s  contains %d Packets.\n",argv[1],number_of_packets);
+      }
+      for(int i=1;i<=number_of_packets;i++){
+	printf("===>Packet %d\n",i);
+	int packet_length;
+	fread(&packet_length,sizeof(int),1,file);
+	printf("Packet length is %d\n ",packet_length);
+	buffer = (char*)malloc(packet_length);
+	int buffer_size = sizeof(buffer);
+	fread(buffer,sizeof(char),packet_length,file);
+	char version = buffer[0]>>4;
+	printf("Its supposed to be a version %#hhx (%d)",version,version);
+	buffer_size=0;
+	free(buffer);
+      }
+      
+      
       fclose(file);
    
       
